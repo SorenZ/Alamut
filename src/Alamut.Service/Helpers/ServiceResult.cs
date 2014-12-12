4db83a82552788390f732d24e8fd5ed3149d3a51
Soren Z.Vand <sunrise.freedom@gmail.com>
2014-12-12 14:33:46 +0330

    - Add Service Projects. - Add Service Result Helper.

---
+++ src/Alamut.Service/Helpers/ServiceResult.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Alamut.Service.Helpers
+{
+    /// <summary>
+    /// Represent Service result data structure
+    /// It can be used as web service result
+    /// </summary>
+    /// <remarks>result for void Service</remarks>
+    [DataContract]
+    public class ServiceResult
+    {
+        [DataMember(Name = "status")]
+        public ResultStatus Status { get; set; }
+
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// return a successful ServiceResult 
+        /// </summary>
+        /// <returns>successful ServiceResult</returns>
+        public static ServiceResult Okay()
+        {
+            return new ServiceResult
+            {
+                Status = ResultStatus.Okay
+            };
+        }
+
+        /// <summary>
+        /// return a ServiceResult with error status and error message
+        /// </summary>
+        /// <param name="message">error message</param>
+        /// <returns>Error ServiceResult</returns>
+        public static ServiceResult Error(string message)
+        {
+            return new ServiceResult
+            {
+                Status = ResultStatus.Error,
+                Message = message
+            };
+        }
+
+        /// <summary>
+        /// return a ServiceResult from exception
+        /// most inmportant exception included in error message
+        /// </summary>
+        /// <param name="ex">the exception</param>
+        /// <returns>Error ServiceResult</returns>
+        public static ServiceResult Error(Exception ex)
+        {
+            return new ServiceResult
+            {
+                Status = ResultStatus.Error,
+                Message = ex.GetBaseException().Message
+            };
+        }
+    }
+
+    /// <summary>
+    /// provides a generic data weapper for Service result 
+    /// </summary>
+    /// <typeparam name="T">data type of return</typeparam>
+    /// <remarks>result for not void result</remarks>
+    public class ServiceResult<T> : ServiceResult
+    {
+        [DataMember(Name = "data")]
+        public T Data { get; set; }
+
+        /// <summary>
+        /// returns a successful typed ServiceResult
+        /// </summary>
+        /// <param name="data">return data</param>
+        /// <returns>successful ServiceResult</returns>
+        public static ServiceResult<T> Okay(T data)
+        {
+            return new ServiceResult<T>
+            {
+                Status = ResultStatus.Okay,
+                Data = data
+            };
+        }
+
+        /// <summary>
+        /// returns a typed ServiceResult with an error
+        /// </summary>
+        /// <param name="message">error message</param>
+        /// <returns>error ServiceResult</returns>
+        public new static ServiceResult<T> Error(string message)
+        {
+            return new ServiceResult<T>
+            {
+                Status = ResultStatus.Error,
+                Message = message
+            };
+        }
+    }
+
+    /// <summary>
+    /// result type status
+    /// </summary>
+    public enum ResultStatus
+    {
+        /// <summary>
+        /// the service got the message and execute it.
+        /// </summary>
+        Okay = 1,
+
+        /// <summary>
+        /// the Service executed and there is some information about it.
+        /// </summary>
+        Information = 2,
+
+        /// <summary>
+        /// the Service executed and there is a warning.
+        /// </summary>
+        Warning = 3,
+
+        /// <summary>
+        /// the commad execution has been failed with error. 
+        /// </summary>
+        Error = 4,
+
+        /// <summary>
+        /// the Service executed successfully 
+        /// </summary>
+        Successful = 5,
+    }
+}
