36ee1e7608729347815d2403e027d141679dfb98
Soren Z.Vand <sunrise.freedom@gmail.com>
2014-12-29 14:20:32 +0330

    Add EnumExtensions. Add GetDescription in EnumExtensions.

---
+++ src/Alamut.Helpers/EnumHelper/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace Alamut.Helpers.EnumHelper
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// provide the value of Description of an enum type
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string GetDescription(this System.Enum enumValue)
+        {
+            var fi = enumValue.GetType().GetField(enumValue.ToString());
+
+            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Description : enumValue.ToString();
+        }
+    }
+}
