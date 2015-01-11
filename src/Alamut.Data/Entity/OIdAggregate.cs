878687086e1e24cefbbc79e583ce256e162deec0
Soren Z.Vand <sunrise.freedom@gmail.com>
2015-01-11 15:50:11 +0330

    add OidAggregate

---
+++ src/Alamut.Data/Entity/OIdAggregate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DF.Core.Models;
+using MongoDB.Bson;
+
+namespace Alamut.Data.Entity
+{
+    public class OIdAggregate : IAggregate<string>
+    {
+        public OIdAggregate()
+        {
+            this.Id = ObjectId.GenerateNewId().ToString();
+        }
+
+        [MaxLength(24)]
+        public string Id { get; set; }
+    }
+}
