145b211ab5d8ee0efe4d21646ffe9bbcc50377d9
Soren Z.Vand <sunrise.freedom@gmail.com>
2015-05-06 14:23:52 +0430

    generate unique key by Mongo Object Id

--- src/Alamut.Security/KeyGenerator.cs
+++ src/Alamut.Security/KeyGenerator.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Globalization;
+using MongoDB.Bson;
 
 namespace Alamut.Security
 {
     /// <summary>
     /// simple unique key generator
     /// </summary>
-    public static class KeyGenerator
+    public static class UniqueKeyGenerator
     {
         /// <summary>
         /// generate base-36 key by Datetime Ticks (utc)
         /// </summary>
         /// <returns></returns>
-        public static string KeyByTick()
+        public static string GenerateKeyByTick()
         {
-            return Base36.Encode((ulong) DateTime.UtcNow.Ticks);
+            return Base36.Encode((ulong) DateTime.Now.Ticks);
         }
 
         /// <summary>
@@ -21,7 +23,7 @@ namespace Alamut.Security
         /// </summary>
         /// <param name="key"></param>
         /// <returns></returns>
-        public static string KeyByStr(string key)
+        public static string GenerateKeyByStr(string key)
         {
             return Base36.Encode(Math.Abs(key.GetHashCode()));
         }
@@ -31,9 +33,28 @@ namespace Alamut.Security
         /// </summary>
         /// <param name="hashCode"></param>
         /// <returns></returns>
-        public static string KeyByHash(int hashCode)
+        public static string GenerateKeyByHash(int hashCode)
         {
             return Base36.Encode(Math.Abs(hashCode));
+        }
+
+        /// <summary>
+        /// generate base-36 key by MongoDb object Id
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateKeyByObjectId()
+        {
+            return Base36.Encode(Math.Abs(ObjectId.GenerateNewId().GetHashCode()));
+        }
+
+        public static string GenerateFromSamBegin()
+        {
+            var samBegin = new DateTime(2015, 01, 01);
+            var elapsedTicks = DateTime.Now.Ticks - samBegin.Ticks;
+
+            //return Base36.Encode(int.Parse(elapsedTicks.ToString(CultureInfo.InvariantCulture).Replace("0", "")));
+            return Base36.Encode(elapsedTicks.GetHashCode());
+
         }
     }
 }
