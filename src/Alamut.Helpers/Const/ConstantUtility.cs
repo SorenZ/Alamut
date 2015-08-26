9758419b0c29ac6a937c66b8d0ce35061f607224
Soren Z.Vand <sunrise.freedom@gmail.com>
2015-08-26 12:06:20 +0430

    constant utility -> get const value of object.

--- src/Alamut.Helpers/Const/ConstantUtility.cs
+++ src/Alamut.Helpers/Const/ConstantUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -16,6 +17,21 @@ namespace Alamut.Helpers.Const
         {
             return type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
                 .Where(fi => fi.IsLiteral && !fi.IsInitOnly);
+        }
+
+        /// <summary>
+        /// get value of const property
+        /// </summary>
+        /// <typeparam name="T">type of value</typeparam>
+        /// <param name="source">source object</param>
+        /// <param name="constantName">the name of const property</param>
+        /// <returns>constant value if available otherwise default value of T </returns>
+        public static T GetConstValue<T>(this object source, string constantName) 
+        {
+            var constantInfo = source.GetType().GetConstants()
+                .FirstOrDefault(q => q.Name == constantName);
+
+            return (constantInfo == null) ? default(T) : (T) constantInfo.GetValue(source);
         }
     }
 }
