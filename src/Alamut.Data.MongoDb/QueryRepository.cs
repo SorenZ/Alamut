94c086fb3ff257a7670a05e924c7d30348f5201c
Soren <sunrise.freedom@gmail.com>
2015-12-16 10:07:18 +0330

    segregate command & query Repositories.

---
+++ src/Alamut.Data.MongoDb/QueryRepository.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Alamut.Data.Entity;
+using Alamut.Data.NoSql;
+using Alamut.Data.Paging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Alamut.Data.MongoDb
+{
+    public class QueryRepository<TDocument> : IQueryRepository<TDocument> where TDocument : IEntity
+    {
+        protected readonly IMongoCollection<TDocument> Collection;
+
+        public QueryRepository(IMongoDatabase database)
+        {
+            Collection = database.GetCollection<TDocument>(typeof (TDocument).Name);
+        }
+
+        public IQueryable<TDocument> Queryable
+        {
+            get { return Collection.AsQueryable(); }
+        }
+
+        public TDocument Get(string id)
+        {
+            return Collection.Find(m => m.Id == id).FirstOrDefault();
+        }
+
+        public TDocument Get(Expression<Func<TDocument, bool>> predicate)
+        {
+            return Collection.Find(predicate).FirstOrDefault();
+        }
+
+        public TResult Get<TResult>(string id, Expression<Func<TDocument, TResult>> projection)
+        {
+            return Collection.Find(m => m.Id == id)
+                .Project(projection)
+                .FirstOrDefault();
+        }
+
+        public TResult Get<TResult>(Expression<Func<TDocument, bool>> predicate,
+            Expression<Func<TDocument, TResult>> projection)
+        {
+            return Collection.Find(predicate)
+                .Project(projection)
+                .FirstOrDefault();
+        }
+
+        public object Get(DynammicCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TDocument> GetAll(bool? isDeleted = null)
+        {
+            if (isDeleted == null && typeof(IDeletableEntity).IsAssignableFrom(typeof(TDocument)))
+                isDeleted = false;
+
+            return isDeleted == null
+                ? Collection.Find(new BsonDocument()).ToList()
+                : Collection.Find(new BsonDocument("IsDeleted", isDeleted.Value)).ToList();
+        }
+
+        public List<TDocument> GetMany(Expression<Func<TDocument, bool>> predicate)
+        {
+            return Collection.Find(predicate).ToList();
+        }
+
+        public List<TDocument> GetMany(IEnumerable<string> ids)
+        {
+            return Collection.Find(q => ids.Contains(q.Id)).ToList();
+        }
+
+        public List<TResult> GetMany<TResult>(Expression<Func<TDocument, bool>> predicate,
+            Expression<Func<TDocument, TResult>> projection)
+        {
+            return Collection.Find(predicate).Project(projection).ToList();
+        }
+
+        public IEnumerable<object> GetMany(DynammicCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPaginated<TDocument> GetPaginated(PaginatedCriteria criteria, bool? isDeleted = null)
+        {
+            if (isDeleted == null && typeof(IDeletableEntity).IsAssignableFrom(typeof(TDocument)))
+                isDeleted = false;
+
+            var filter = isDeleted == null
+                ? new BsonDocument()
+                : new BsonDocument("IsDeleted", isDeleted.Value);
+
+            using (var cursor = Collection.Find(filter)
+                .Skip(criteria.StartIndex)
+                .Limit(criteria.PageSize)
+                .ToCursor())
+            {
+                return new Paginated<TDocument>(
+                    cursor.ToEnumerable(),
+                    Collection.Count(new BsonDocument()),
+                    criteria.CurrentPage,
+                    criteria.PageSize);
+            }
+        }
+
+        public IPaginated<object> GetPaginated(DynamicPaginatedCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Alamut.Data/NoSql/IRepository.cs
+++ src/Alamut.Data/NoSql/IRepository.cs
@@ -1,26 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using Alamut.Data.Entity;
-using Alamut.Data.Paging;
 
 namespace Alamut.Data.NoSql
 {
-    public interface IRepository<TDocument> where TDocument : IEntity
+    public interface IRepository<TDocument> : IQueryRepository<TDocument> 
+        where TDocument : IEntity
     {
-        /// <summary>
-        /// Creates a queryable source of documents
-        /// </summary>
-        IQueryable<TDocument> Queryable { get; }
-
         /// <summary>
         /// create an item
         /// </summary>
         /// <param name="entity"></param>
         void Create(TDocument entity);
-
-
+        
         /// <summary>
         /// update item total value
         /// </summary>
@@ -61,7 +54,6 @@ namespace Alamut.Data.NoSql
         /// <param name="id"></param>
         /// <param name="fieldset"></param>
         void GenericUpdate(string id, Dictionary<string, dynamic> fieldset);
-
         
         /// <summary>
         /// add an item to a list (if not exist)
@@ -80,8 +72,7 @@ namespace Alamut.Data.NoSql
         /// <param name="memberExpression"></param>
         /// <param name="value"></param>
         void RemoveFromList<TValue>(string id, Expression<Func<TDocument, IEnumerable<TValue>>> memberExpression, TValue value);
-
-
+        
         /// <summary>
         /// Deletes an item by id.
         /// </summary>
@@ -93,117 +84,11 @@ namespace Alamut.Data.NoSql
         /// </summary>
         /// <param name="predicate">represent expression to filter delete</param>
         void DeleteMany(Expression<Func<TDocument, bool>> predicate);
-
-
+        
         /// <summary>
         /// set is deleted to true by id
         /// </summary>
         /// <param name="id"></param>
         void SetDeleted(string id);
-
-        /// <summary>
-        /// get an item by id 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        TDocument Get(string id);
-
-        /// <summary>
-        /// get an item by predicate
-        /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
-        TDocument Get(Expression<Func<TDocument, bool>> predicate);
-
-        /// <summary>
-        /// get an item (selected fields bye projection) by id
-        /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="id"></param>
-        /// <param name="projection"></param>
-        /// <returns></returns>
-        TResult Get<TResult>(string id, Expression<Func<TDocument, TResult>> projection);
-
-        /// <summary>
-        /// get an item (selected fields bye projection) by predicate
-        /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="predicate"></param>
-        /// <param name="projection"></param>
-        /// <returns></returns>
-        TResult Get<TResult>(Expression<Func<TDocument, bool>> predicate, 
-            Expression<Func<TDocument, TResult>> projection);
-
-        /// <summary>
-        /// get one item by filters
-        /// support by Id query
-        /// </summary>
-        /// <param name="criteria"></param>
-        /// <returns></returns>
-        object Get(DynammicCriteria criteria);
-
-        /// <summary>
-        /// get all items 
-        /// </summary>
-        /// <param name="isDeleted">
-        /// could be true, false, null
-        /// null -> not important 
-        /// </param>
-        /// <returns></returns>
-        List<TDocument> GetAll(bool? isDeleted = null);
-
-
-        /// <summary>
-        /// get a list of items by predicate
-        /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
-        List<TDocument> GetMany(Expression<Func<TDocument, bool>> predicate);
-
-        /// <summary>
-        /// get a list of items by ids
-        /// </summary>
-        /// <param name="ids"></param>
-        /// <returns></returns>
-        List<TDocument> GetMany(IEnumerable<string> ids);
-
-        /// <summary>
-        /// get a list of items (selected fields) by predicate
-        /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="predicate"></param>
-        /// <param name="projection"></param>
-        /// <returns></returns>
-        List<TResult> GetMany<TResult>(Expression<Func<TDocument, bool>> predicate, 
-            Expression<Func<TDocument, TResult>> projection);
-
-        /// <summary>
-        /// get items (fields or all of them) by filters 
-        /// </summary>
-        /// <param name="criteria"></param>
-        /// <returns></returns>
-        IEnumerable<object> GetMany(DynammicCriteria criteria);
-
-        /// <summary>
-        /// get items paginated by criteria
-        /// </summary>
-        /// <param name="criteria"></param>
-        /// <param name="isDeleted"></param>
-        /// <returns></returns>
-        IPaginated<TDocument> GetPaginated(PaginatedCriteria criteria, bool? isDeleted = null);
-
-        /// <summary>
-        /// get items paginated and filterd and sorted by criteria(s)
-        /// </summary>
-        /// <param name="criteria"></param>
-        /// <returns></returns>
-        IPaginated<object> GetPaginated(DynamicPaginatedCriteria criteria);
     }
-
-
-    //public interface IRepository<TEntity, TKey>
-    //    where TEntity : IEntity<TKey>
-    //{
-
-    //}
 }
