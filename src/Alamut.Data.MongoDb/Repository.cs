78f4bef5d3b1689287fea71266173a7137b0fee9
Soren Z.Vand <sunrise.freedom@gmail.com>
2016-01-17 17:12:52 +0330

    - add Entity Extensions. - add Data Structure Extensions. -

--- src/Alamut.Data.MongoDb/Repository.cs
+++ src/Alamut.Data.MongoDb/Repository.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using Alamut.Data.Entity;
-using Alamut.Data.NoSql;
 using Alamut.Data.Repository;
 using MongoDB.Bson;
 using MongoDB.Driver;
