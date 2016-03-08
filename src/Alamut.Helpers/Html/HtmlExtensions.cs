4961416dd1b241a8192eed447c8b24ee4a59a639
Soren Z.Vand <sunrise.freedom@gmail.com>
2016-03-08 14:15:57 +0330

    update HtmlExtensions.  -add teg stripper.

--- src/Alamut.Helpers/Html/HtmlExtensions.cs
+++ src/Alamut.Helpers/Html/HtmlExtensions.cs
@@ -25,5 +25,36 @@ namespace Alamut.Helpers.Html
                 ? result
                 : result.Substring(0, length.Value) + endWith;
         }
+
+        /// <summary>
+        /// Remove HTML tags from string using char array.
+        /// </summary>
+        public static string StripHtmlTags(this string source)
+        {
+            var array = new char[source.Length];
+            var arrayIndex = 0;
+            var inside = false;
+
+            foreach (var let in source)
+            {
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+
+                if (inside) continue;
+
+                array[arrayIndex] = @let;
+                arrayIndex++;
+            }
+            return new string(array, 0, arrayIndex);
+        }
     }
 }
